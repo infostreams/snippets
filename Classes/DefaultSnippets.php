32582f3514dea6d081c73e40a02f9ed87b8109fd
Edward Akerboom <github@infostreams.net>
2014-12-23 17:40:10 +0100

    Moved common functionality into base Snippets class

--- Classes/DefaultSnippets.php
+++ Classes/DefaultSnippets.php
@@ -1,7 +1,7 @@
 <?php
 namespace Phile\Plugin\Infostreams\Snippets;
 
-class DefaultSnippets implements Snippets {
+class DefaultSnippets extends Snippets {
 
 	/**
 	 * Implements the 'link' snippet
@@ -166,102 +166,4 @@ class DefaultSnippets implements Snippets {
 		return "<script src='$link'></script>";
 	}
 
-
-
-
-
-	/* internal functions */
-
-	/**
-	 * Determines whether or not a user-provided value evaluates to 'true'.
-	 *
-	 * This is the case whenever the provided value is "yes", "true" or "1".
-	 *
-	 * @param $value
-	 * @return bool
-	 */
-	private function isTrue($value) {
-		$value = strtolower(trim((string)$value));
-		return (in_array($value, array("yes", "true", "1")));
-	}
-
-	/**
-	 * Determines whether or not a user-provided value evaluates to 'false'.
-	 * This is the case when 'isTrue()' does NOT evaluate to 'true'.
-	 *
-	 * @param $value
-	 * @return bool
-	 */
-	private function isFalse($value) {
-		return !$this->isTrue($value);
-	}
-
-	/**
-	 * Turns internal links into proper absolute links, and/or adds 'http'
-	 * to external links that were specified without one.
-	 *
-	 * @param $link
-	 * @return string
-	 */
-	private function getLink($link) {
-		$url_parts = parse_url($link);
-
-		if (is_array($url_parts)) {
-			if (!array_key_exists("scheme", $url_parts)) {
-				// we have a partially incomplete URL here
-				// -> see if we just have to add 'http://'
-				//    or if we are dealing with an internal link
-				$p = explode('/', $url_parts["path"]);
-				$domain = $p[0];
-				if ($this->isValidDomainName($domain)) {
-					// the first part of the link is a valid domain name
-					// -> just prepend 'http://' and continue
-					return "http://$link";
-				} else {
-					// this is an internal link
-					return \Phile\Utility::getBaseUrl() . '/'. ltrim($link, '/');
-				}
-			}
-		}
-
-		// PHP could not parse the link, which means it's probably invalid. However,
-		// in this case, we just return whatever the user typed.
-		return $link;
-	}
-
-	/**
-	 * Checks if the given string is a valid domain name
-	 * @param $domain_name
-	 * @return bool
-	 */
-	private function isValidDomainName($domain_name)
-	{
-		return (preg_match('/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i', $domain_name) //valid chars check
-			&& preg_match('/^.{1,253}$/', $domain_name) //overall length check
-			&& preg_match('/^[^\.]{1,63}(\.[^\.]{1,63})*$/', $domain_name)   ) //length of each label
-			&& count(explode('.', $domain_name))>1;
-	}
-
-
-	/**
-	 * Turns a list of attributes provided as an associative array into an equivalent string
-	 * of HTML attributes that can be inserted into a HTML tag. Skips any attributes that
-	 * are considered empty.
-	 *
-	 * @param $attributes
-	 * @return string
-	 */
-	private function getHtmlAttributes($attributes) {
-		$list = array();
-		foreach ($attributes as $name=>$value) {
-			if (!empty($value)) {
-				$list[] = "$name='" . htmlentities($value, ENT_QUOTES, "UTF-8") . "'";
-			}
-		}
-		if (count($list)>0) {
-			return " " . implode(" ", $list);
-		}
-
-		return "";
-	}
 }
